4a4d59e616b57c15ae218fa9e43986233ff609e2
Rebecca Appleyard <rklappleyard@gmail.com>
2015-10-21 15:38:48 +0100

    Contacts showing on button click - all tests passing

--- js/contactsController.js
+++ js/contactsController.js
@@ -1,3 +1,32 @@
 addressBookApp.controller('ContactsController', [function() {
+  var self = this;
 
+
+
+  self.showAllContacts = function() {
+    self.contactsList = {
+    "contacts" : [
+      {
+        first_name: "Gareth",
+        surname: "Billington",
+        address: "Universal Marina",
+        phone_number: "0121212",
+        email: "gareth.billington@alliants.com",
+        id: 6791,
+        createdAt: "2015-04-29T20:10:26.000Z",
+        updatedAt: "2015-09-29T08:55:21.000Z"
+      },
+      {
+        first_name: "K",
+        surname: "Z",
+        address: "asfasd",
+        phone_number: "123123",
+        email: "a@wp.pl",
+        id: 6891,
+        createdAt: "2015-06-02T11:09:19.000Z",
+        updatedAt: "2015-09-24T18:51:52.000Z"
+      },
+    ]
+  }
+  }
 }]);
