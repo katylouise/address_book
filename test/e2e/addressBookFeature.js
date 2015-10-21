bf78dff83dce60875cd0def882385cbe525ac8e7
Rebecca Appleyard <rklappleyard@gmail.com>
2015-10-21 19:04:17 +0100

    Protractor tests passing now for single contact display

--- test/e2e/addressBookFeature.js
+++ test/e2e/addressBookFeature.js
@@ -1,12 +1,21 @@
 describe('Address Book', function() {
+  var contacts = element.all(by.repeater('contact in contactCtrl.contactsList'));
+  var searchBox = element(by.model('contactCtrl.searchTerm'));
+  var searchButton = element(by.className('search-button'));
+
   it('displays the title', function() {
     browser.get('http://localhost:8080');
     expect(browser.getTitle()).toEqual('Address Book');
   });
 
   it('shows all contacts', function() {
-    var contacts = element.all(by.repeater('contact in contactCtrl.contactsList'));
     expect(contacts.get(0).element(by.binding('contact.first_name')).getText()).toEqual('Gareth');
+  });
+
+  it('can display a single contact', function() {
+    searchBox.sendKeys('John');
+    searchButton.click();
+    expect(element(by.binding('contactCtrl.contact.first_name')).getText()).toEqual('John');
   });
 });
 
