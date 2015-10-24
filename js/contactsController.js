25821e1f4a7b1798ed3efa6bb8292a66c2e1bec3
Rebecca Appleyard <rklappleyard@gmail.com>
2015-10-24 12:30:40 +0100

    Refactored some of the vars in controller

--- js/contactsController.js
+++ js/contactsController.js
@@ -3,17 +3,17 @@ addressBookApp.controller('ContactsController', ['$resource', 'UpdateContact', f
   var contactsResource = $resource('https://fast-gorge.herokuapp.com/contacts');
   var Contact = $resource('https://fast-gorge.herokuapp.com/contacts/:id', { id: '@id' });
   var position;
+  var contactID;
   self.submitted = false;
   self.contact;
-  self.contactID;
   self.contactsList = contactsResource.query();
 
   self.searchForContact = function() {
     for (var i = 0; i < self.contactsList.length; i++) {
       if (self.contactsList[i].first_name === self.searchTerm || self.contactsList[i].surname === self.searchTerm) {
         position = i;
-        self.contactID = self.contactsList[i].id;
-        self.contact = Contact.get({ id: self.contactID });
+        contactID = self.contactsList[i].id;
+        self.contact = Contact.get({ id: contactID });
         self.showSingleContact = true;
       }
     }
@@ -57,7 +57,7 @@ addressBookApp.controller('ContactsController', ['$resource', 'UpdateContact', f
   }
 
   self.deleteContact = function() {
-    Contact.remove({ id: self.contactID }).$promise.then(function() {
+    Contact.remove({ id: contactID }).$promise.then(function() {
       self.result = "Contact deleted!";
       self.contactsList.splice(position, 1);
       self.showSingleContact = false;
